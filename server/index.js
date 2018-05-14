3847ff09b143bdfe455a04bf39e41c3b552c8bf6
eedaihee <eedaihee@gmail.com>
2018-05-14 23:11:26 +0900

    Node.js server

--- server/index.js
+++ server/index.js
@@ -1,13 +1,14 @@
 import http from 'http'
 
-const port = 3000
+const server = http.createServer()
+const PORT = 3000
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200
-  res.setHeader('Content-Type', 'text/plain')
-  res.end('Hello World')
+server.on('request', (req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'})
+  res.write('Hello World')
+  res.end()
 })
 
-server.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}/`)
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`)
 })
