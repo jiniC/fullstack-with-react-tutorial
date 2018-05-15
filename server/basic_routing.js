4f7b4b0375db9187346dc35c9ee6a9efd6a478c1
eedaihee <daihee.ee@nts-corp.com>
2018-05-15 17:37:38 +0900

    Express

---
+++ server/basic_routing.js
@@ -0,0 +1,24 @@
+import express from 'express'
+
+const app = express()
+const PORT = 3001
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.post('/', (req, res) => {
+  res.send('Got a POST request')
+})
+
+app.put('/user', (req, res) => {
+  res.send('Got a PUT request at /user')
+})
+
+app.delete('/user', (req, res) => {
+  res.send('Got a DELETE request at /user')
+})
+
+app.listen(PORT, () => {
+  console.log(`Express server is running at http://localhost:${PORT}`)
+})
