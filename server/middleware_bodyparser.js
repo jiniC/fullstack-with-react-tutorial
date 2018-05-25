bfc6e790746dd5dce23cdec7034ddd991e6b284b
eedaihee <eedaihee@gmail.com>
2018-05-25 23:08:10 +0900

    Express

--- server/middleware_bodyparser.js
+++ server/middleware_bodyparser.js
@@ -3,7 +3,6 @@ import bodyParser from 'body-parser'
 
 const app = express()
 const PORT = 3001
-const router = express.Router()
 
 app.use(express.static('public'))
 
@@ -13,17 +12,15 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // body-parser를 사용해 application/json 파싱
 app.use(bodyParser.json())
 
-router.post('/process/login', (req, res) => {
-  console.log('/process/login 호출함')
+app.post('/login', (req, res) => {
+  console.log('/login 호출함')
 
-  const paramId = req.body.id || req.query.id
-  const paramPassword = req.body.password || req.query.password
+  const paramId = req.body.id
+  const paramPassword = req.body.password
 
   res.writeHead(200, {'Content-Type': 'text/html;charset=utf8'})
   res.end(`입력한 id는 ${paramId}, password는 ${paramPassword} 입니다.`)
 })
-
-app.use('/', router)
 
 app.listen(PORT, () => {
   console.log(`Express server is running at http://localhost:${PORT}`)
--- server/middleware_cookieparser.js
+++ server/middleware_cookieparser.js
@@ -3,33 +3,18 @@ import cookieParser from 'cookie-parser'
 
 const app = express()
 const PORT = 3001
-const router = express.Router()
 
 app.use(cookieParser())
 
-router.get('process/showCookie', (req, res) => {
-  console.log('/process/showCookie 호출함')
-
-  // res.writeHead(200, {'Content-Type': 'text/html;charset=utf8'})
-  res.send(req.cookies.user)
+app.get('/', (req, res) => {
+  res.cookie('myCookie', 'good')
+  res.end('Set cookie')
 })
 
-router.get('/process/setUserCookie', (req, res) => {
-  console.log('/process/setUserCookie 호출함')
-
-  // 쿠키 설정
-  res.cookie('user', {
-    id: 'eedaihee',
-    name: '이대희',
-    authorized: true
-  })
-
-  // res.send(req.cookies)
-
-  res.redirect('/process/showCookie')
+app.get('/removeCookie', (req, res) => {
+  res.clearCookie('myCookie')
+  res.end('Remove cookie')
 })
-
-app.use('/', router)
 
 app.listen(PORT, () => {
   console.log(`Express server is running at http://localhost:${PORT}`)
